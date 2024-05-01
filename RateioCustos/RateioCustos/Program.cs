20da11050cba2e4a209f7a7a2560c67333fa98d8
chyltonmr <chylton.rezende@hotmail.com>
2024-04-30 21:08:57 -0300

    Inicio aplicacao de rateio de custos

---
+++ RateioCustos/RateioCustos/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RateioCustoFrete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, (double preco, int quantidade)> produtos = new Dictionary<string, (double, int)>();
+
+            // Solicita os produtos, preços e quantidades até que o usuário decida parar
+            while (true)
+            {
+                Console.WriteLine("Digite o nome do produto (ou digite 'fim' para finalizar):");
+                string nomeProduto = Console.ReadLine();
+
+                // Verifica se o usuário digitou 'fim' para encerrar a entrada de produtos
+                if (nomeProduto.ToLower() == "fim")
+                    break;
+
+                Console.WriteLine("Digite o preço do produto:");
+                double precoProduto;
+
+                // Verifica se o preço é válido
+                while (!double.TryParse(Console.ReadLine(), out precoProduto))
+                {
+                    Console.WriteLine("Preço inválido! Por favor, insira um valor numérico válido:");
+                }
+
+                Console.WriteLine("Digite a quantidade do produto:");
+                int quantidadeProduto;
+
+                // Verifica se a quantidade é válida
+                while (!int.TryParse(Console.ReadLine(), out quantidadeProduto))
+                {
+                    Console.WriteLine("Quantidade inválida! Por favor, insira um valor numérico válido:");
+                }
+
+                // Adiciona o produto, seu preço e sua quantidade ao dicionário
+                produtos.Add(nomeProduto, (precoProduto, quantidadeProduto));
+            }
+
+            // Solicita o custo total do frete
+            Console.WriteLine("Digite o custo total do frete:");
+            double custoFrete = Convert.ToDouble(Console.ReadLine());
+
+            // Calcula o preço total dos produtos
+            double precoTotal = 0;
+            foreach (var produto in produtos)
+            {
+                precoTotal += produto.Value.preco * produto.Value.quantidade;
+            }
+
+            // Calcula o custo do frete para cada produto e o custo total de cada produto
+            foreach (var produto in produtos)
+            {
+                double percentual = (produto.Value.preco * produto.Value.quantidade / precoTotal) * 100;
+                double custoFreteProduto = (custoFrete * percentual) / 100;
+                double custoTotalProduto = produto.Value.preco + (custoFreteProduto / produto.Value.quantidade);
+
+                Console.WriteLine($"Custo total do Produto '{produto.Key}' (incluindo frete): R$ {custoTotalProduto:F2}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
